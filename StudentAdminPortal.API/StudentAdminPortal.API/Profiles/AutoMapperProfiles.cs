d4ab61df6cb8eb04940c22596dea0d97a8e0ed71
TonyJP9x <phamdinhthuan1995@gmail.com>
2023-04-10 16:43:10 +0700

    second commit

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DomainModels;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, Student>();
+            CreateMap<DataModels.Gender, Gender>();
+            CreateMap<DataModels.Address, Address>();
+        }
+    }
+}
